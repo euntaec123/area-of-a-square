4b97218a49d6c695dbce67d9e1549aa57d57b2df
euntaec123 <61697376+euntaec123@users.noreply.github.com>
2020-03-16 16:32:38 +0200

    Add files via upload

---
+++ area of a square.py
@@ -0,0 +1,19 @@
+#Name: Euntaec Hong
+#Date:16/03/2020
+
+
+# asking value from the user
+
+length=eval(input("Please enter your length \n"))
+
+#asking for second value
+
+breadth=eval(input("Please enter your breath \n"))
+
+area= length*breadth
+
+print(" Your are is", area)
+
+#exiting
+
+Quit=eval(input("Please Press any to exit \n"))
